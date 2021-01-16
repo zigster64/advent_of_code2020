07218e60de0aff275452e141ef6087641496739e
zigster64 <steve@raffmail.net>
2021-01-16 12:35:56 +1000

    day9 - part 1 tests passing

--- day9/src/main.zig
+++ day9/src/main.zig
@@ -6,23 +6,69 @@ var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 var allocator = &gpa.allocator;
 
 const xvType = u64;
+const offsetType = u8;
 const xmasValues = std.ArrayList(xvType);
 const xmas = struct {
     values: xmasValues,
+    preambleSize: offsetType,
 
     fn deinit(self: xmas) void {
         self.values.deinit();
     }
+
+    fn firstFailingNumber(self: xmas) ?xvType {
+        var i: ?xvType = null;
+        var offset: offsetType = self.preambleSize;
+        var len = self.values.items.len;
+        while (offset < len) : (offset += 1) {
+            var v = self.values.items[offset];
+            //print("check value {}:{}\n", .{ offset, v });
+            var ok = self.checkValue(offset);
+            if (!ok) {
+                //print("false\n", .{});
+                return v;
+            }
+            //print("true\n", .{});
+        }
+        return i;
+    }
+
+    fn checkValue(self: xmas, offset: offsetType) bool {
+        var lookingFor = self.values.items[offset];
+        var seekOffset = offset - self.preambleSize;
+        var i: offsetType = seekOffset;
+        while (i < offset) : (i += 1) {
+            var j: offsetType = seekOffset;
+            while (j < offset) : (j += 1) {
+                if ((i != j) and (self.values.items[i] + self.values.items[j] == lookingFor)) {
+                    //print("{}:{}  {}+{}={} is equal !! {}\n", .{
+                    //i,                                           j,
+                    //self.values.items[i],                        self.values.items[j],
+                    //self.values.items[i] + self.values.items[j], lookingFor,
+                    //});
+                    return true;
+                } else {
+                    //print("{}:{}  {}+{}={} != {}\n", .{
+                    //i,                                           j,
+                    //self.values.items[i],                        self.values.items[j],
+                    //self.values.items[i] + self.values.items[j], lookingFor,
+                    //});
+                }
+            }
+        }
+        return false;
+    }
 };
 
-fn newXmas(alloc: *std.mem.Allocator, values: string) anyerror!xmas {
-    var x = xmas{ .values = xmasValues.init(alloc) };
+fn newXmas(alloc: *std.mem.Allocator, values: string, ps: offsetType) anyerror!xmas {
+    var x = xmas{ .values = xmasValues.init(alloc), .preambleSize = ps };
     var lines = std.mem.tokenize(values, "\n");
     while (lines.next()) |line| {
         var i = try std.fmt.parseUnsigned(xvType, line, 10);
         try x.values.append(i);
+        //print("Value {}\n", .{i});
     }
-    print("done {}\n", .{x});
+    std.debug.assert(x.values.items.len > x.preambleSize);
 
     return x;
 }
@@ -30,14 +76,17 @@ fn newXmas(alloc: *std.mem.Allocator, values: string) anyerror!xmas {
 pub fn main() anyerror!void {
     std.log.info("day 9 - XMAS decoder\n", .{});
 
-    var x = try newXmas(allocator, @embedFile("input.data"));
+    var x = try newXmas(allocator, @embedFile("input.data"), 25);
     defer x.deinit();
 }
 
 test "test5" {
     print("test 5\n", .{});
 
-    var x = try newXmas(allocator, @embedFile("test.data"));
-    print("here\n", .{});
+    var x = try newXmas(allocator, @embedFile("test.data"), 5);
     defer x.deinit();
+
+    var ff = x.firstFailingNumber().?;
+    print("first fail = {}\n", .{ff});
+    expect(ff == 127);
 }
